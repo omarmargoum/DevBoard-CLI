2c2a67481c7f12d6a32ebde1d5595125cb16d51d
Margoum Omar <omar.margoum@gmail.com>
2026-09-11 09:43:45 +0200

    notre premier pas en git

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log("DevBoard Démarre");
+const applicationName = "DevBoard CLI";
+console.log(`${applicationName} démarre`);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+console.log("DevBoard Démarre");
+const applicationName: string = "DevBoard CLI";
+
+console.log(`${applicationName} démarre`);
