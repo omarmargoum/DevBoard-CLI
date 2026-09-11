88d54ef16eb1455a65d42eb29f57e27283dc7c36
Margoum Omar <omar.margoum@gmail.com>
2026-09-11 09:56:13 +0200

    Exercice 2

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-console.log("DevBoard Démarre");
-const applicationName = "DevBoard CLI";
-console.log(`${applicationName} démarre`);
+/*console.log("DevBoard Démarre");
+const applicationName: string = "DevBoard CLI";
+
+console.log(`${applicationName} démarre`);*/
+/*const developerName = "Omar Margoum";
+const city = "Verviers";
+let experienceYears = 20;
+let available = true;
+let weeklyHours = 12;
+console.log(`Nom du développeur : ${developerName}`);
+console.log(`Ville : ${city}`);
+console.log(`Expérience : ${experienceYears} ans`);
+console.log(`Disponible : ${available}`);
+console.log(`Charge disponible : ${weeklyHours} heures par semaine`);*/
+const developerName = "Omar Margoum";
+const city = "Verviers";
+const currentYear = 2026;
+let startYear = 2024;
+const experienceYears = currentYear - startYear;
+let available = true;
+let weeklyHours = 12;
+console.log("=== DEVBOARD CLI ===");
+console.log(`Nom          : ${developerName}`);
+console.log(`Ville        : ${city}`);
+console.log(`Expérience   : ${experienceYears} ans`);
+console.log(`Disponible   : ${available ? "oui" : "non"}`);
+console.log(`Charge       : ${weeklyHours} h/semaine`);
+console.log("====================");
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,32 @@
-console.log("DevBoard Démarre");
+/*console.log("DevBoard Démarre");
 const applicationName: string = "DevBoard CLI";
 
-console.log(`${applicationName} démarre`);
+console.log(`${applicationName} démarre`);*/
+const developerName: string = "Omar Margoum";
+const city: string = "Verviers";
+let experienceYears: number = 20;
+let available: boolean = true;
+let weeklyHours: number = 12;
+
+console.log(`Nom du développeur : ${developerName}`);
+console.log(`Ville : ${city}`);
+console.log(`Expérience : ${experienceYears} ans`);
+console.log(`Disponible : ${available}`);
+console.log(`Charge disponible : ${weeklyHours} heures par semaine`);
+const developerName: string = "Omar Margoum";
+const city: string = "Verviers";
+
+const currentYear: number = 2026;
+let startYear: number = 2024;
+const experienceYears: number = currentYear - startYear;
+
+let available: boolean = true;
+let weeklyHours: number = 12;
+
+console.log("=== DEVBOARD CLI ===");
+console.log(`Nom          : ${developerName}`);
+console.log(`Ville        : ${city}`);
+console.log(`Expérience   : ${experienceYears} ans`);
+console.log(`Disponible   : ${available ? "oui" : "non"}`);
+console.log(`Charge       : ${weeklyHours} h/semaine`);
+console.log("====================");
